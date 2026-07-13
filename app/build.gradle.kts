6b819d14ffd1f57327626e268795c9c650a61202
MajorTomMan <47267913+MajorTomMan@users.noreply.github.com>
2026-07-13 22:37:50 +0800

    版本：升级到 0.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.majortomman.school"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "0.15.0"
+        versionCode = 15
+        versionName = "0.16.0"
     }
 
     buildTypes {
