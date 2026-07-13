59767643c6220b35d1b0f2d94bc71370437ea8fe
MajorTomMan <47267913+MajorTomMan@users.noreply.github.com>
2026-07-13 11:59:17 +0800

    版本：升级极简场景预览到 preview2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.majortomman.school"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.6.0-preview1"
+        versionCode = 7
+        versionName = "0.6.0-preview2"
     }
 
     buildTypes {
