aeadcc9682d48f10ec039fd3d65e9efdfdd98ceb
MajorTomMan <47267913+MajorTomMan@users.noreply.github.com>
2026-07-14 22:50:28 +0800

    版本：升级到 0.18.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.majortomman.school"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.17.0"
+        versionCode = 17
+        versionName = "0.18.0"
     }
 
     buildTypes {
